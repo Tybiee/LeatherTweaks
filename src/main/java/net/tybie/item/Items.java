178543f5585bbb013fe87bf0856d07827ed007e3
Tybie <tybiemeyer@gmail.com>
2020-11-27 21:03:35 -0500

    Crimson Armor Code Added

--- src/main/java/net/tybie/item/Items.java
+++ src/main/java/net/tybie/item/Items.java
@@ -1,11 +1,10 @@
 package net.tybie.item;
 
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.MushroomStewItem;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.tybie.armor.CrimsonLeather;
 
 public class Items {
 
@@ -14,6 +13,13 @@ public class Items {
     public static final Item WARPED_LEATHER = new Item(new Item.Settings().group(ItemGroup.MISC));
     public static final Item CRIMSON_LEATHER_STRIP = new Item(new Item.Settings().group(ItemGroup.MISC));
     public static final Item WARPED_LEATHER_STRIP = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+    // Infected Leather Armor
+    public static final ArmorMaterial customArmorMaterial = new CrimsonLeather();
+    public static final Item CRIMSON_HELMET = new ArmorItem(customArmorMaterial, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item CRIMSON_CHESTPLATE = new ArmorItem(customArmorMaterial, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item CRIMSON_LEGGINGS = new ArmorItem(customArmorMaterial, EquipmentSlot.LEGS, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item CRIMSON_BOOTS = new ArmorItem(customArmorMaterial, EquipmentSlot.FEET, new Item.Settings().group(ItemGroup.COMBAT));
 
     // Misc Items
     public static final Item LEATHER_STRIP = new Item(new Item.Settings().group(ItemGroup.MISC));
@@ -25,10 +31,18 @@ public class Items {
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_leather"), (CRIMSON_LEATHER));
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "warped_leather"), (WARPED_LEATHER));
+
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "leather_strip"), (LEATHER_STRIP));
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_leather_strip"), (CRIMSON_LEATHER_STRIP));
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "warped_leather_strip"), (WARPED_LEATHER_STRIP));
+
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_stew"), (CRIMSON_STEW));
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "warped_stew"), (WARPED_STEW));
+
+        Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_helmet"), (CRIMSON_HELMET));
+        Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_chestplate"), (CRIMSON_CHESTPLATE));
+        Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_leggings"), (CRIMSON_LEGGINGS));
+        Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_boots"), (CRIMSON_BOOTS));
+
     }
 }
