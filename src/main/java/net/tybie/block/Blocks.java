bdb0e725894f7a8ee92fba83d85556a440ba7eba
Tybie <tybiemeyer@gmail.com>
2020-12-31 21:14:52 -0500

    Cutting Table Implemented

--- src/main/java/net/tybie/block/Blocks.java
+++ src/main/java/net/tybie/block/Blocks.java
@@ -19,6 +19,9 @@ public class Blocks {
     public static final Block CRIMSON_LEATHER_BLOCK = new Block(FabricBlockSettings.of(Material.WOOL).strength(0.8f).sounds (BlockSoundGroup.WOOL));
     public static final Block WARPED_LEATHER_BLOCK = new Block(FabricBlockSettings.of(Material.WOOL).strength(0.8f).sounds (BlockSoundGroup.WOOL));
 
+    // Misc Code
+    public static final Block CUTTING_TABLE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f).sounds (BlockSoundGroup.STONE));
+
     // Registry
     public static void register() {
         Registry.register(Registry.BLOCK, new Identifier("leathertweaks", "leather_block"), LEATHER_BLOCK);
@@ -27,6 +30,8 @@ public class Blocks {
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "crimson_leather_block"), new BlockItem(CRIMSON_LEATHER_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
         Registry.register(Registry.BLOCK, new Identifier("leathertweaks", "warped_leather_block"), WARPED_LEATHER_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("leathertweaks", "warped_leather_block"), new BlockItem(WARPED_LEATHER_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.BLOCK, new Identifier("leathertweaks", "cutting_table"), CUTTING_TABLE);
+        Registry.register(Registry.ITEM, new Identifier("leathertweaks", "cutting_table"), new BlockItem(CUTTING_TABLE, new Item.Settings().group(ItemGroup.DECORATIONS)));
 
     }
 }
